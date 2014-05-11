63547eedd8542f2f083e1ddf0fadf287682d6d0e
blivesta <enmt@blivesta.com>
2014-05-11 12:46:50 +0900

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,395 @@
+ module.exports = function(grunt) {
+
+   "use strict";
+
+   require('load-grunt-tasks')(grunt);
+   require('time-grunt')(grunt);
+
+   grunt.initConfig({
+
+     pkg: grunt.file.readJSON('package.json'),
+
+     banner: '/*!\n' +
+       ' * <%= pkg.name %> v<%= pkg.version %>\n' +
+       ' * <%= pkg.url %>\n' +
+       ' * Licensed under <%= pkg.licenses %>\n' +
+       ' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+       ' * <%= pkg.authorUrl %>\n' +
+       ' */\n',
+     // ====================================================
+     clean: {
+       files: [
+         '<%= pkg.dist %>',
+         '<%= pkg.source %>/js/<%= pkg.name %>.js',
+         '<%= pkg.source %>/js/<%= pkg.name %>.min.js',
+         '<%= pkg.public %>'
+       ]
+     },
+     // ====================================================
+     less: {
+       source: {
+         options: {
+           strictMath: true,
+           sourceMap: true,
+           outputSourceFiles: true,
+           sourceMapURL: ['<%= pkg.name %>.css.map'],
+           sourceMapFilename: '<%= pkg.source %>/css/<%= pkg.name %>.css.map'
+         },
+         files: {
+           '<%= pkg.source %>/css/<%= pkg.name %>.css': '<%= pkg.source %>/less/<%= pkg.name %>.less'
+         }
+       },
+       minify: {
+         options: {
+           cleancss: true
+         },
+         files: {
+           '<%= pkg.source %>/css/<%= pkg.name %>.min.css': '<%= pkg.source %>/css/<%= pkg.name %>.css'
+         }
+       }
+     },
+     // ====================================================
+     autoprefixer: {
+       options: {
+         browsers: ['last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12']
+       },
+       source: {
+         options: {
+           map: true
+         },
+         src: '<%= pkg.source %>/css/<%= pkg.name %>.css'
+       }
+     },
+     // ====================================================
+     csscomb: {
+       options: {
+         config: '<%= pkg.source %>/less/.csscomb.json'
+       },
+       source: {
+         expand: true,
+         cwd: '<%= pkg.source %>/css/',
+         src: ['*.css', '!*.min.css'],
+         dest: '<%= pkg.source %>/css/'
+       }
+     },
+     // ====================================================
+     usebanner: {
+       options: {
+         position: 'top',
+         banner: '<%= banner %>'
+       },
+       source: {
+         src: '<%= pkg.source %>/css/*.css'
+       }
+     },
+     // ====================================================
+     csslint: {
+       options: {
+         csslintrc: '<%= pkg.source %>/less/.csslintrc'
+       },
+       source: [
+         '<%= pkg.source %>/css/<%= pkg.name %>.css'
+       ]
+     },
+     // ====================================================
+     concat: {
+       options: {
+         banner: '<%= banner %>',
+         stripBanners: false
+       },
+       source: {
+         src: [
+           '<%= pkg.source %>/js/plugin.js',
+         ],
+         dest: '<%= pkg.source %>/js/<%= pkg.name %>.js'
+       }
+     },
+     // ====================================================
+     uglify: {
+       options: {
+         banner: '<%= banner %>',
+         report: 'min',
+         mangle: false,
+         compress: false
+       },
+       source: {
+         options: {
+           indentLevel: 2,
+           beautify: true
+         },
+         files: {
+           '<%= pkg.source %>/js/<%= pkg.name %>.js': [
+             '<%= pkg.source %>/js/<%= pkg.name %>.js'
+           ]
+         }
+       },
+       // minify:{
+       //   files :  { 
+       //     '<%= pkg.source %>/js/<%= pkg.name %>.min.js' : [
+       //       '<%= pkg.source %>/js/<%= pkg.name %>.js' 
+       //     ]
+       //   } 
+       // }
+     },
+     // ====================================================
+     jshint: {
+       options: {
+         jshintrc: '<%= pkg.source %>/js/.jshintrc',
+       },
+       grunt: {
+         src: 'Gruntfile.js'
+       },
+       source: {
+         src: [
+           '<%= pkg.source %>/js/<%= pkg.name %>.js',
+       //    '<%= pkg.source %>/js/<%= pkg.name %>.min.js'
+         ]
+       }
+     },
+     // ====================================================
+     jscs: {
+       options: {
+         config: '<%= pkg.source %>/js/.jscsrc'
+       },
+       grunt: {
+         options: {
+           requireCamelCaseOrUpperCaseIdentifiers: null,
+           requireParenthesesAroundIIFE: true
+         },
+         src: '<%= jshint.grunt.src %>'
+       },
+       source: {
+         src: '<%= jshint.source.src %>'
+       }
+     },
+     // ====================================================
+     htmlmin: {
+       publish: {
+         options: {
+           collapseBooleanAttributes: true,
+           collapseWhitespace: true,
+           removeAttributeQuotes: true,
+           removeCommentsFromCDATA: true,
+           removeEmptyAttributes: true,
+           removeOptionalTags: true,
+           removeRedundantAttributes: true,
+           useShortDoctype: true
+         },
+         files: [{
+           expand: true,
+           cwd: '<%= pkg.public %>',
+           src: '{,*/}*.html',
+           dest: '<%= pkg.public %>'
+         }]
+       }
+     },
+     // ====================================================
+     validation: {
+       options: {
+         charset: 'utf-8',
+         doctype: 'HTML5',
+         failHard: true,
+         reset: true,
+         relaxerror: [
+           'Bad value X-UA-Compatible for attribute http-equiv on element meta.',
+           'Element img is missing required attribute src.'
+         ]
+       },
+       files: {
+         src: [
+           '<%= pkg.public %>/index.html',
+           '<%= pkg.public %>/**/*.html'
+         ]
+       }
+     },
+     // ====================================================
+     copy: {
+       dist: {
+         expand: true,
+         cwd: './<%= pkg.source %>',
+         src: [
+           'js/<%= pkg.name %>.js',
+           'js/<%= pkg.name %>.min.js',
+           'css/*.css',
+           'css/*.map'
+         ],
+         dest: './<%= pkg.dist %>'
+       }
+     },
+     // ====================================================
+     connect: {
+       server: {
+         options: {
+           port: 9999,
+           hostname: '0.0.0.0',
+           base: '<%= pkg.public %>/',
+           open: {
+             server: {
+               path: 'http://<%= connect.server.options.hostname %>:<%= connect.server.options.port %>'
+             }
+           }
+         }
+       }
+     },
+     // ====================================================
+     notify: {
+       options: {
+         title: '<%= pkg.name %> Grunt Notify',
+       },
+       success: {
+         options: {
+           message: 'Success!',
+         }
+       }
+     },
+     // ====================================================
+     bower: {
+       install: {
+         options: {
+           targetDir: '<%= pkg.source %>/vendor',
+           layout: 'byComponent',
+           install: true,
+           verbose: false,
+           cleanTargetDir: true,
+           cleanBowerDir: false
+         }
+       }
+     },
+     // ====================================================
+     jekyll: {
+       dist: {
+         options: {
+           config: '_config.yml'
+         }
+       }
+     },
+     // ====================================================
+     watch: {
+       options: {
+         spawn: false,
+         livereload: true
+       },
+       grunt: {
+         files: ['<%= jshint.grunt.src %>'],
+         tasks: [
+           'jshint:grunt',
+           'notify'
+         ]
+       },
+       js: {
+         files: [
+           '<%= pkg.source %>/js/*.js'
+         ],
+         tasks: [
+           'build-js',
+           'jshint:source',
+           //'jscs:source',
+           'jekyll',
+           'notify'
+         ]
+       },
+       html: {
+         files: [
+           '<%= pkg.source %>/*.html',
+           '<%= pkg.source %>/_includes/*',
+           '<%= pkg.source %>/_posts/*',
+           '<%= pkg.source %>/_layouts/*'
+         ],
+         tasks: [
+           'build-html',
+           'notify'
+         ]
+       },
+       less: {
+         files: [
+           '<%= pkg.source %>/less/*.less',
+           '<%= pkg.source %>/less/**/*.less'
+         ],
+         tasks: [
+           'build-less',
+           'jekyll',
+           'notify'
+         ]
+       }
+     },
+     // ====================================================
+     buildcontrol: {
+       options: {
+         dir: '<%= pkg.public %>',
+         commit: true,
+         push: true,
+         message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+       },
+       pages: {
+         options: {
+           remote: 'git@github.com:<%= pkg.repository.user %>/<%= pkg.name %>.git',
+           branch: 'gh-pages'
+         }
+       }
+     }
+
+   });
+
+   //publicに指定したディレクトリをgh-pagesブランチにデプロイ。
+   // ====================================================
+   grunt.registerTask('deploy', [
+     'buildcontrol',
+     'notify'
+   ]);
+
+   // lessコンパイル
+   // ====================================================
+   grunt.registerTask('build-less', [
+     'less:source',
+     'autoprefixer:source',
+     'usebanner',
+     'csscomb:source',
+     'less:minify',
+   ]);
+
+   // jsコンパイル
+   // ====================================================
+   grunt.registerTask('build-js', [
+     'concat',
+     'uglify'
+   ]);
+
+   // jekyllコンパイル
+   // ====================================================
+   grunt.registerTask('build-html', [
+     'jekyll',
+     //'htmlmin'
+   ]);
+
+   // js,css,htmlのテスト
+   // ====================================================
+   grunt.registerTask('test', [
+     'jshint:source',
+     //'jscs:source',
+     'csslint',
+     'validation'
+   ]);
+
+   // ベンダーファイルのインストール →　コンパイル　→　テスト　→　ウォッチ
+   // ====================================================
+   grunt.registerTask('b', [
+     'clean',
+     'bower',
+     'build-less',
+     'build-js',
+     'build-html',
+     'test',
+     'copy'
+   ]);
+
+   // サーバー起動　→　ウオッチ
+   // ====================================================
+   grunt.registerTask('default', function() {
+     grunt.log.warn('`grunt` to start a watch.');
+     grunt.task.run([
+       'connect',
+       'watch'
+     ]);
+   });
+
+ };
---
+++ README.md
@@ -0,0 +1,86 @@
+drawer
+===
+Drawer is a jQuery plugin for creating a responsive, touch-enabled and css animated side menu that slides out from the edge of the screen when clicking or touching the trigger. 
+
+![drawer](./src/images/drawer-small.gif)
+![drawer-image](./src/images/drawer-image-small.jpg)
+
+##example
+http://blivesta.github.io/drawer/
+
+##Setup
+~~~ go
+<!-- vendor CSS -->
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
+<!-- drawer css -->
+<link rel="stylesheet" href="./dist/css/drawer.min.css">
+<!-- Vendor js -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- cooker.drawer -->
+<script src="./dist/js/drawer.js"></script>
+
+<script type="text/javascript">
+$('.drawer').drawer({
+  // options
+  masta:        "drawer-masta"
+  nav:          "drawer-nav",
+  navList:      "drawer-nav-list",
+  overlay:      "drawer-overlay",
+  toggle:       "drawer-toggle",
+  body:         "drawer-body",
+  openClass:    "drawer-open",
+  closeClass:   "drawer-close",
+  desktopEvent: "click", // or mouseover
+  width:        280
+});
+</script>
+
+<body class="drawer"> 
+  <button class="drawer-toggle btn">
+    <i class="glyphicon glyphicon-align-justify"></i>
+  </button>
+  <div class="drawer-masta drawer-default">
+    <nav class="drawer-nav" role="navigation">
+      <div class="drawer-brand">
+        <a href="#">Drawer</a>
+      </div>
+      <ul class="nav drawer-nav-list">
+        <li>
+          <a href="#">...</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  <div class="drawer-overlay">
+    contents...
+  </div>
+</body>
+~~~
+
+##API Methods
+####Open
+~~~ go
+$('#element').drawer('open');
+~~~
+
+####Close
+~~~ go
+$('#element').drawer('close');
+~~~
+
+####Destroy
+~~~ go
+$('#element').drawer('destroy');
+~~~ 
+
+##Browsers
+- IE 10 +
+- Firefox
+- opera
+- Mac safari
+- iOS7 safari
+- iOS7 chrome
+
+##License
+Released under the MIT license.
+
