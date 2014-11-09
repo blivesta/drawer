ab9f3ad789412c91400fac46a6aa50e79cc60b07
blivesta <enmt@blivesta.com>
2014-11-09 15:23:09 +0900

    Fixed transitionEnd function

--- src/js/drawer.js
+++ src/js/drawer.js
@@ -127,39 +127,15 @@
   
 })(jQuery);
 
-
-/*!
- * csscallbacks v0.0.1
- * http://blivesta.github.io/csscallbacks/
- * Licensed under MIT
- * Author : blivesta
- * http://blivesta.com/
- */
 (function($) {
   "use strict";
-  $.fn.transitionEnd = function(callback) {
-    var $this = $(this);
-    var props = "transitionend webkitTransitionEnd mozTransitionEnd oTransitionEnd MSTransitionEnd";
-    if ($this.length > 0) {
-      $this.bind(props, function(event) {
-        if ($.isFunction(callback)) {
-          callback.call($this, event);
-        }
+  $.fn.transitionEnd = function(callback){
+    var end = "transitionend webkitTransitionEnd mozTransitionEnd oTransitionEnd MSTransitionEnd";
+    return this.each(function() {
+      $(this).bind(end, function(){
+        $(this).unbind(end);
+        return callback.call(this);
       });
-    }
-    return $this;
+    });
   };
-  $.fn.animationEnd = function(callback) {
-    var $this = $(this);
-    var props = "animationend webkitAnimationEnd mozAnimationEnd oAnimationEnd MSAnimationEnd";
-    if ($this.length > 0) {
-      $this.bind(props, function(event) {
-        if ($.isFunction(callback)) {
-          callback.call($this, event);
-        }
-      });
-    }
-    return $this;
-  };
-    
 })(jQuery);
