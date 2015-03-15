ad6b0557fe7466bc2d770bcfb2d96b223c923e62
blivesta <enmt@blivesta.com>
2015-03-16 00:35:07 +0900

    fix uninstall task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var browsersync = require('browser-sync');
 var reload      = browsersync.reload;
 var gulp        = require('gulp');
-var del         = require('del');
 var runSequence = require('run-sequence');
 var pkg         = require('./package.json');
 var autoprefixerBrowsers = [
@@ -93,9 +92,7 @@ gulp.task('deploy', require('gulptasks/lib/ghpage'));
 gulp.task('bump', require('gulptasks/lib/bump'));
 gulp.task('jekyll', require('gulptasks/lib/jekyll'));
 gulp.task('browsersync', require('gulptasks/lib/browsersync'));
-gulp.task('uninstall',function() {
-  return del(module.exports.uninstall.files);
-});
+gulp.task('uninstall', require('gulptasks/lib/uninstall'));
 
 gulp.task('default',['browsersync'],function() {
   gulp.watch(['./src/scss/*.scss'], ['rubysass']);
