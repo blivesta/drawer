e9592d9e11f77c35be00245f3d5830d5d3179c5d
blivesta <enmt@blivesta.com>
2015-03-16 00:04:58 +0900

    Update 2015-03-15T15:04:57.819Z

--- drawer/jquery.drawer.js
+++ drawer/jquery.drawer.js
@@ -1,9 +1,9 @@
 /*!
- * drawer v2.4.0
- * http://git.blivesta.com/drawer
- * Licensed under MIT
- * Author : blivesta
- * http://blivesta.com
+ * jquery-drawer - A small CSS3 and jQuery plugin for app style drawer menu.
+ * @version v2.4.0
+ * @link    http://git.blivesta.com/drawer
+ * @author  blivesta
+ * @license MIT
  */
 (function($) {
   "use strict";
@@ -12,48 +12,56 @@
   var methods = {
     init: function(options) {
       options = $.extend({
-        mastaClass: "drawer-main",
-        toggleClass: "drawer-toggle",
-        upperClass: "drawer-overlay-upper",
-        openClass: "drawer-open",
-        closeClass: "drawer-close",
-        apiToggleClass: "drawer-api-toggle",
-        responsiveClass: "drawer-responsive",
-        dropdownClass: "dropdown",
-        dropdownShown: "shown.bs.dropdown",
-        dropdownHidden: "hidden.bs.dropdown"
+        mastaClass:        "drawer-main",
+        toggleClass:       "drawer-toggle",
+        upperClass:        "drawer-overlay-upper",
+        openClass:         "drawer-open",
+        closeClass:        "drawer-close",
+        apiToggleClass:    "drawer-api-toggle",
+        responsiveClass:   "drawer-responsive",
+        dropdownClass:     "dropdown",
+        dropdownShown:     "shown.bs.dropdown",
+        dropdownHidden:    "hidden.bs.dropdown"
       }, options);
       return this.each(function() {
         var _this = this;
         var $this = $(this);
         var data = $this.data(namespace);
-        var $upper = $("<div>").addClass(options.upperClass + " " + options.toggleClass);
+        var $upper = $("<div>").addClass(options.upperClass+" "+options.toggleClass);
+
         if (!data) {
           options = $.extend({}, options);
           $this.data(namespace, {
             options: options
           });
         }
+
         $this.append($upper);
-        var drawerScroll = new IScroll("." + options.mastaClass, {
-          mouseWheel: true,
+
+        var drawerScroll = new IScroll("."+options.mastaClass, {
+          mouseWheel:true,
           preventDefault: false
         });
-        $("." + options.toggleClass + ", ." + options.apiToggleClass).off("click." + namespace).on("click." + namespace, function() {
-          methods.toggle.call(_this);
-          drawerScroll.refresh();
-        });
+
+        $("." + options.toggleClass + ", ." + options.apiToggleClass)
+          .off("click." + namespace)
+          .on("click." + namespace, function() {
+            methods.toggle.call(_this);
+            drawerScroll.refresh();
+          });
+
         $(window).resize(function() {
           methods.close.call(_this);
           drawerScroll.refresh();
         });
-        $("." + options.dropdownClass).on(options.dropdownShown, function() {
-          drawerScroll.refresh();
-        }).on(options.dropdownHidden, function() {
-          drawerScroll.refresh();
-        });
-      });
+
+        $("." + options.dropdownClass)
+        .on(options.dropdownShown, function() { drawerScroll.refresh(); })
+        .on(options.dropdownHidden, function() { drawerScroll.refresh(); });
+
+      }); // end each
     },
+
     toggle: function(options) {
       var _this = this;
       var $this = $(this);
@@ -61,6 +69,7 @@
       var open = $this.hasClass(options.openClass);
       open ? methods.close.call(_this) : methods.open.call(_this);
     },
+
     open: function(options) {
       var $this = $(this);
       options = $this.data(namespace).options;
@@ -69,24 +78,32 @@
           event.preventDefault();
         });
       }
-      $this.removeClass(options.closeClass).addClass(options.openClass).transitionEnd(function() {
-        $this.css({
-          overflow: "hidden"
-        }).trigger("drawer.opened");
-      });
+      $this
+        .removeClass(options.closeClass)
+        .addClass(options.openClass)
+        .transitionEnd(function(){
+          $this.css({
+            "overflow": "hidden"
+          }).trigger('drawer.opened');
+        });
     },
+
     close: function(options) {
       var $this = $(this);
       options = $this.data(namespace).options;
       if (touches) {
         $this.off("touchmove." + namespace);
       }
-      $this.removeClass(options.openClass).addClass(options.closeClass).transitionEnd(function() {
-        $this.css({
-          overflow: "auto"
-        }).trigger("drawer.closed");
-      });
+      $this
+        .removeClass(options.openClass)
+        .addClass(options.closeClass)
+        .transitionEnd(function(){
+          $this.css({
+            "overflow": "auto"
+          }).trigger('drawer.closed');
+        });
     },
+
     destroy: function() {
       return this.each(function() {
         var $this = $(this);
@@ -94,7 +111,9 @@
         $this.removeData(namespace);
       });
     }
+
   };
+
   $.fn.drawer = function(method) {
     if (methods[method]) {
       return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -104,14 +123,15 @@
       $.error("Method " + method + " does not exist on jQuery." + namespace);
     }
   };
+
 })(jQuery);
 
 (function($) {
   "use strict";
-  $.fn.transitionEnd = function(callback) {
+  $.fn.transitionEnd = function(callback){
     var end = "transitionend webkitTransitionEnd mozTransitionEnd oTransitionEnd MSTransitionEnd";
     return this.each(function() {
-      $(this).bind(end, function() {
+      $(this).bind(end, function(){
         $(this).unbind(end);
         return callback.call(this);
       });
