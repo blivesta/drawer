beb575ca3b9b7b658e7c0f835287581bc6765bd6
ungki <ungki.h@gmail.com>
2015-08-10 20:15:09 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,120 +1,77 @@
-var browsersync = require('browser-sync');
-var reload      = browsersync.reload;
-var gulp        = require('gulp');
+'use strict';
+
+var cssnext = require('gulp-cssnext');
+var del = require('del');
+var gulp = require('gulp');
+var header = require('gulp-header');
+var jshint = require('gulp-jshint');
+var pkg = require('./package.json');
+var rename = require('gulp-rename');
 var runSequence = require('run-sequence');
-var pkg         = require('./package.json');
-var autoprefixerBrowsers = [
-  'Android 2.3',
-  'Android >= 4',
-  'Chrome >= 20',
-  'Firefox >= 24', // Firefox 24 is the latest ESR
-  'Explorer >= 8',
-  'iOS >= 6',
-  'Opera >= 12',
-  'Safari >= 6'
-]
-var headerBanner = [
+var stylish = require('jshint-stylish');
+var uglify = require('gulp-uglify');
+
+var banner = [
 '/*!',
-' * <%= pkg.name %> - <%= pkg.description %>',
-' * @version v<%= pkg.version %>',
-' * @link    <%= pkg.homepage %>',
-' * @author  <%= pkg.author %>',
-' * @license <%= pkg.license %>',
+' * <%= pkg.name %> v<%= pkg.version %>',
+' * <%= pkg.description %>',
+' * <%= pkg.homepage %>',
+' * License : <%= pkg.license %>',
+' * Author : <%= pkg.author %>',
 ' */',
 ''].join('\n');
 
-module.exports = {
-  'browsersync': {
-    server: './_gh_pages'
-  },
-  'uninstall': {
-    files: [
-      'dist',
-      './docs/drawer',
-      './_gh_pages'
-    ]
-  },
-  'rubysass': {
-    src: './src/scss/drawer.scss',
-    dest: './docs/drawer',
-    rubySassOptions: {
-      sourcemap: true,
-      noCache: true
-    },
-    autoprefixer: autoprefixerBrowsers,
-    fallback:false,
-    filter:'**/*.css',
-    headerBanner : true,
-    banner:headerBanner,
-    pkg: pkg,
-    notify :"Compiled Sass"
-  },
-  'cssmin': {
-    src: './dist/css/drawer.css',
-    dest: './dist/css'
-  },
-  'jshint': {
-    setting: './src/js/.jshintrc',
-    src:  './dist/js/*.js'
-  },
-  'jsmin': {
-    src:  './dist/js/jquery.drawer.js',
-    dest: './dist/js'
-  },
-  'banner': {
-    src:  './src/js/jquery.drawer.js',
-    dest:  './docs/drawer',
-    pkg: pkg,
-    banner:headerBanner
-  },
-  'ghpage' : {
-    src : './_gh_pages/**/*',
-    remoteUrl : 'https://github.com/blivesta/drawer.git',
-    branch : 'gh-pages'
-  },
-  'bump': {
-    version: pkg.version,
-    src:  './bower.json',
-    dest: '.'
-  }
+var dirs = {
+  src:'./src',
+  dist:'./dist'
 };
 
-gulp.task('bower', require('gulptasks/lib/bower'));
-gulp.task('rubysass', require('gulptasks/lib/rubysass'));
-gulp.task('jshint', require('gulptasks/lib/jshint'));
-gulp.task('cssmin', require('gulptasks/lib/cssmin'));
-gulp.task('jsmin', require('gulptasks/lib/jsmin'));
-gulp.task('banner', require('gulptasks/lib/banner'));
-gulp.task('deploy', require('gulptasks/lib/ghpage'));
-gulp.task('bump', require('gulptasks/lib/bump'));
-gulp.task('jekyll', require('gulptasks/lib/jekyll'));
-gulp.task('browsersync', require('gulptasks/lib/browsersync'));
-gulp.task('uninstall', require('gulptasks/lib/uninstall'));
-
-gulp.task('default',['browsersync'],function() {
-  gulp.watch(['./src/scss/*.scss'], ['rubysass']);
-  gulp.watch(['./src/js/*.js'], ['banner']);
-  gulp.watch(['./docs/**/*.{html,css,js,md}'], ['jekyll',reload]);
-  gulp.watch(['./_public'], reload);
+gulp.task('css', function () {
+  return gulp
+    .src(dirs.src + '/css/drawer.css')
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(cssnext({
+      browsers: ['last 2 versions'],
+      sourcemap: true
+    }))
+    .pipe(gulp.dest(dirs.dist + '/css'))
+    .pipe(cssnext({
+      compress: true,
+      sourcemap: false
+    }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(dirs.dist + '/css'));
 });
 
-gulp.task('distCss',function() {
-  return gulp.src('./docs/drawer/*.css')
-    .pipe(gulp.dest('./dist/css'));
+gulp.task('js', function(){
+  return gulp
+    .src(dirs.src + '/js/drawer.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(header(banner, { pkg:pkg }))
+    .pipe(rename({prefix: 'jquery.'}))
+    .pipe(gulp.dest(dirs.dist + '/js'))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(header(banner, { pkg:pkg }))
+    .pipe(gulp.dest(dirs.dist + '/js'));
 });
 
-gulp.task('distJs',function() {
-  return gulp.src('./docs/drawer/*.js')
-    .pipe(gulp.dest('./dist/js'));
+gulp.task('cleanup', function(){
+  return del([ './dist' ]);
 });
 
-gulp.task('build', function() {
+gulp.task('default',['build'], function(){
+  gulp.watch(['./src/css/**/*.css'], ['css']);
+  gulp.watch(['./src/js/*.js'], ['js']);
+});
+
+gulp.task('build', function(){
   runSequence(
-    'bower', 'uninstall','bump',
-    ['rubysass','banner'],
-    'distCss','distJs',
-    ['cssmin','jsmin','jekyll'],
-    'jshint',
-    ['default']
+    'cleanup',
+    ['js'],
+    'css'
   );
 });
